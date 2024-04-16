a95c0407eec3e87716100f58c4b9f4fdc60aa83c
Yan Ye <evelynye0102@gmail.com>
2024-04-15 21:26:42 -0400

    commit

--- server.py
+++ server.py
@@ -1,16 +1,34 @@
 from flask import Flask, render_template
 
 
-
-
 app = Flask(__name__)
 
-@app.route('/')
+quiz1_answer = []
+
+quiz2_answer = []
+
+quiz3_answer = []
+
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+@app.route("/quiz/1")
+def quiz1_submit():
+    return render_template("index.html")
+
+
+@app.route("/quiz/2")
+def quiz2_submit():
+    return render_template("index.html")
+
+
+@app.route("/quiz/3")
+def quiz3_submit():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-
---
+++ static/quiz1.js
@@ -0,0 +1,4 @@
+/* Add javscript here */
+$(document).ready(function() {
+  
+});
