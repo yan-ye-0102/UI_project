359ff1130e0acfb92081457c3ab3ca4ca0a5e466
Yan Ye <evelynye0102@gmail.com>
2024-04-26 20:49:26 -0400

    add answers

--- helpers.py
+++ helpers.py
@@ -1,13 +1,11 @@
-CORRECT_ANSWERS = ["c"] * 5
-
 CORRECT_ANSWER = [
-    [(390, 190), (373, 434), (566, 257), (552, 474), (693, 461)],
-    [(442, 216), (608, 239), (351, 433), (503, 427), (697, 422)],
-    [(569, 115), (662, 235), (451, 450), (613, 382), (777, 456)],
+    [(403, 179), (567, 254), (384, 437), (552, 471), (703, 462)],
+    [(443, 191), (603, 233), (358, 423), (499, 428), (695, 425)],
+    [(554, 121), (666, 239), (438, 441), (612, 378), (777, 456)],
     "c",
 ]
 
-THRETHOLD = 5
+THRETHOLD = 1000
 
 
 def extract_positions(request):
@@ -17,31 +15,27 @@ def extract_positions(request):
     # 'position_3': {'left': 45, 'top': 250},
     # 'position_4': {'left': 45, 'top': 375},
     # 'position_5': {'left': 45, 'top': 500}}
-    print(
-        [(request.json[pos]["left"], request.json[pos]["top"]) for pos in request.json]
-    )
+
     return [
         (request.json[pos]["left"], request.json[pos]["top"]) for pos in request.json
     ]
 
 
 def check_answer(my_answer, correct):
-    # calculate the absolute distance and direction to the pg
+    result = 0
     for i in range(len(my_answer)):
         dist = (my_answer[i][0] - correct[i][0]) ** 2 + (
             my_answer[i][1] - correct[i][1]
         ) ** 2
-        if dist > THRETHOLD:
-            return False
-    return True
-
+        if dist <= THRETHOLD:
+            result += 0.5
+    return result
 
 def get_final_score(answers):
     final_score = 0
     for i, answer in enumerate(answers):
         if isinstance(answer, list):
-            if check_answer(answer, CORRECT_ANSWER[i]):
-                final_score += 2.5
+            final_score += check_answer(answer, CORRECT_ANSWER[i])
         else:
             if answer == CORRECT_ANSWER[i]:
                 final_score += 2.5
